dd1ddac95c18c375d5fc0f110db9ec65f3a88fbb
Lamfo <me@lamfo.ru>
2021-08-29 20:28:55 +0000

    new file:   install.sh 	new file:   profile

---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+### CHECK
+ls /dev/loop0 > /dev/null || ( echo 'Ты псих?' && exit 1 )
+
+### CLEAR
+swapoff -a
+umount /mnt/boot
+umount /mnt
+
+### NETWORK
+echo 'nameserver 1.1.1.1' > /etc/resolv.conf
+dev=$(ip -br a show | grep UP | awk '{print $1}')
+while [[ ! `ping -c 1 1.1`  ]]; do
+  read -p 'IP: ' ip
+  read -p 'GW: ' gw
+  ip add add $ip dev $dev
+  ip ro add 0.0.0.0/0 via $gw
+done
+
+
+### DISK
+disk=$(fdisk -l | grep '^Disk /dev/' | sort -hk 3 | head -n1 | awk '{print $2}' | tr -d ':')
+echo "Выполняю установку на диск $disk"
+
+dd if=/dev/zero of=${disk} bs=8M count=256 > /dev/null 
+
+parted $disk -s  mklabel msdos
+parted $disk -s mkpart primary 1M 512M
+parted $disk -s mkpart primary 512M 100%
+parted $disk set 1 boot on
+
+mkfs.ext2 ${disk}1 -F
+mkfs.ext4 ${disk}2 -F
+
+mount ${disk}2 /mnt
+mkdir /mnt/boot
+mount ${disk}1 /mnt/boot
+
+dd if=/dev/zero of=/mnt/swap bs=8M count=64 > /dev/null 
+chmod 0600 /mnt/swap
+mkswap /mnt/swap 
+swapon /mnt/swap 
+
+pacstrap /mnt base base-devel grub sudo git make curl wget htop vim net-tools ethtool openssh wireguard-tools
+
+genfstab -U /mnt > /mnt/etc/fstab
+echo 'LANG=en_US.UTF-8' > /mnt/etc/locale.conf
+echo -e 'en_US.UTF-8 UTF-8\nru_RU.UTF-8 UTF-8' > /etc/locale.gen
+
+arch-chroot /mnt /bin/bash <<"EOT"
+  disk=$(fdisk -l | grep '^Disk /dev/' | sort -hk 3 | head -n1 | awk '{print $2}' | tr -d ':')
+  grub-install ${disk}
+  grub-mkconfig -o /boot/grub2/grub.cfg
+  
+  timedatectl set-timezone Europe/Moscow
+  locale-gen
+  
+  curl 'https://raw.githubusercontent.com/lamfo-dev/ssh/main/fastadd.sh' | bash --
+  systemctl enable sshd
+  sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+  sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+  
+  useradd -c lamfo -g users -G wheel lamfo
+  
+  pwd
+EOT
+
+
---
+++ profile
@@ -0,0 +1,56 @@
+# colors
+darkgrey="$(tput bold ; tput setaf 1)"
+white="$(tput bold ; tput setaf 7)"
+blue="$(tput bold; tput setaf 4)"
+cyan="$(tput bold; tput setaf 6)"
+nc="$(tput sgr0)"
+
+# exports
+export PATH="${HOME}/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:"
+export PATH="${PATH}/usr/local/sbin:/opt/bin:/usr/bin/core_perl:/usr/games/bin:"
+#export PS1="\[$blue\][ \[$cyan\]\H \[$darkgrey\]\w\[$darkgrey\] \[$blue\]]\\[$darkgrey\]$ \[$nc\]"
+export PS1='\[\e[0;32m\][\A]\[\e[0m\]:\[\e[0;31m\][\u@\h \[\e[0;94m\]\[\e[0;31m\]\W]\$ \[\e[0m\]'
+export LD_PRELOAD=""
+export EDITOR="vim"
+#export MSF_DATABASE_CONFIG="`ls ~/.msf4/database.yml`"
+export CALICO_DATASTORE_TYPE=kubernetes
+export CALICO_KUBECONFIG=~/.kube/config
+
+# alias
+
+alias ls="ls --color"
+alias l="ls -la --color"
+alias t="tree -Csh"
+alias vi="vim"
+alias shred="shred -zf"
+alias wget="wget -U 'noleak'"
+alias curl="curl --user-agent 'noleak'"
+alias ap="ansible-playbook"
+alias ail="ansible-inventory --list"
+alias an="ansible"
+alias anr="ansible -m reboot"
+alias lsblk="lsblk -o NAME,FSTYPE,LABEL,MOUNTPOINT,MODEL"
+# source files
+[ -r /usr/share/bash-completion/completions ] &&
+  . /usr/share/bash-completion/completions/*
+
+function getpkgsize(){
+	LC_ALL=C pacman -Qi | awk '/^Name/{name=$3} /^Installed Size/{print $4$5, name}' | sort -h
+}
+
+linstor()(kubectl -n piraeus-system exec -it piraeus-controller-0 -- linstor $@)
+
+complete -C /usr/bin/mcli mcli
+
+declare -x HISTCONTROL="ignoredups"
+declare -x HISTFILESIZE="10000"
+declare -x HISTSIZE="10000"
+declare -x HISTTIMEFORMAT="%F %T "
+shopt -s histappend
+PROMPT_COMMAND='history -a'
+
+export TESSDATA_PREFIX=/usr/share/
+
+getresizespeed()(echo $(( (0 - $(ls -la $1 | awk '{print $5}') + $(sleep 10 && ls -la $1 | awk '{print $5}'))/10/1024/1024))МБ/c)
+
+#export http_proxy='http://user:user@ip:port'
